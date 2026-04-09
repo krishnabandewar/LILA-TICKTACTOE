48ac77dbafb3df9a1793ba131db12ff58fd544cc
krishnabandewar-turing <krishnabandewar43@gmial.com>
2026-04-09 13:20:18 +0530

    Cleanup: removed logs and test scripts

--- nakama/modules/test.lua
+++
@@ -1,7 +0,0 @@
-local nk = require("nakama")
-
-local function test_rpc_fn(context, payload)
-  return nk.json_encode({ ok = true, msg = "test works!" })
-end
-
-nk.register_rpc(test_rpc_fn, "test_rpc")
