87ed2c6e8c935fe145b70b1fb07dfe8b43ea6f38
krishnabandewar-turing <krishnabandewar43@gmial.com>
2026-04-11 12:25:38 +0530

    Fix: use shell script entrypoint for reliable env var expansion

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
-echo "Starting database migration..."
 /nakama/nakama migrate up --database.address "$NK_DATABASE_ADDRESS"
-echo "Starting Nakama server..."
 exec /nakama/nakama \
   --database.address "$NK_DATABASE_ADDRESS" \
   --runtime.path /nakama/data/modules \
