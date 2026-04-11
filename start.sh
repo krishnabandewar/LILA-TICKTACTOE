29f36d8db37a1457d0f8a64018a3e80c60f3e7b5
krishnabandewar-turing <krishnabandewar43@gmial.com>
2026-04-11 12:31:32 +0530

    Fix: add CORS headers via socket.response_headers

--- start.sh
+++ start.sh
@@ -3,4 +3,8 @@ set -e
 /nakama/nakama migrate up --database.address "$NAKAMA_DATABASE_ADDRESS"
 exec /nakama/nakama \
   --database.address "$NAKAMA_DATABASE_ADDRESS" \
-  --runtime.path /nakama/data/modules
+  --runtime.path /nakama/data/modules \
+  --socket.response_headers "Access-Control-Allow-Origin: *" \
+  --socket.response_headers "Access-Control-Allow-Headers: Authorization,Content-Type,*" \
+  --socket.response_headers "Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS" \
+  --socket.response_headers "Access-Control-Expose-Headers: *"
