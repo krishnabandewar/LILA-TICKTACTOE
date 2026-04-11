e94ca53c69899573700fb6056be9f0796103ffcc
krishnabandewar-turing <krishnabandewar43@gmial.com>
2026-04-11 12:28:33 +0530

    Fix: remove invalid cors flag, use NAKAMA_DATABASE_ADDRESS

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
-/nakama/nakama migrate up --database.address "$NK_DATABASE_ADDRESS"
+/nakama/nakama migrate up --database.address "$NAKAMA_DATABASE_ADDRESS"
 exec /nakama/nakama \
-  --database.address "$NK_DATABASE_ADDRESS" \
-  --runtime.path /nakama/data/modules \
-  --session.cors_allowed_origins "*"
+  --database.address "$NAKAMA_DATABASE_ADDRESS" \
+  --runtime.path /nakama/data/modules
