e1b41d4d519bbb4faaa58fe4eb6585aa7057f5df
krishnabandewar-turing <krishnabandewar43@gmial.com>
2026-04-11 12:33:10 +0530

    Fix: use key=value format for response headers

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 exec /nakama/nakama \
   --database.address "$NAKAMA_DATABASE_ADDRESS" \
   --runtime.path /nakama/data/modules \
-  --socket.response_headers "Access-Control-Allow-Origin: *" \
-  --socket.response_headers "Access-Control-Allow-Headers: Authorization,Content-Type,*" \
-  --socket.response_headers "Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS" \
-  --socket.response_headers "Access-Control-Expose-Headers: *"
+  --socket.response_headers "Access-Control-Allow-Origin=*" \
+  --socket.response_headers "Access-Control-Allow-Headers=Authorization,Content-Type,*" \
+  --socket.response_headers "Access-Control-Allow-Methods=GET,POST,PUT,DELETE,OPTIONS" \
+  --socket.response_headers "Access-Control-Expose-Headers=*"
